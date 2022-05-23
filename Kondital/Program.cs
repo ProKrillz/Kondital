22e94b0f51307cd27fe98ceca25369956f1f3c63
ProKrillz <thomasdamkjaer@gmail.com>
2022-05-23 11:12:57 +0200

    class med metode

--- Kondital/Program.cs
+++ Kondital/Program.cs
@@ -2,20 +2,41 @@
 double hvilePuls = WriteDouble("Hvilepuls: ");
 double maxPuls = WriteDouble("Maxpuls: ");
 
+Kondital test = new Kondital(WriteDouble("Weigt in kilo: "), WriteDouble("Hvilepuls: "), WriteDouble("Maxpuls: "));
+Console.WriteLine(test.BeregnKondital());
+
 static double WriteDouble(string text)
 {
-    double kilo;
+    double value;
     while (true)
     {
         Console.Write(text);
         try
         {
-            return kilo = Convert.ToDouble(Console.ReadLine());
+            return value = Convert.ToDouble(Console.ReadLine());
         }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
         }
+    }
+}
+
+class Kondital
+{
+    double _kilo;
+    double _hvilePuls;
+    double _maxPuls;
+    
+    public Kondital(double kilo, double hvilePuls, double maxPuls)
+    {
+        this._kilo = kilo; 
+        this._hvilePuls = hvilePuls;
+        this._maxPuls = maxPuls;
+    }
+    public double BeregnKondital()
+    {
+        return (_maxPuls/ _hvilePuls) * 15.3;
     }
 }
 
